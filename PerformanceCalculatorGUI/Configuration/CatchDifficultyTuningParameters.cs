e2f5b471849facf1a4108c6ac9aae990a655a966
vernonlim <vernonlim2004@gmail.com>
2026-02-15 22:35:37 +0800

    Fix

--- PerformanceCalculatorGUI/Configuration/CatchDifficultyTuningParameters.cs
+++ PerformanceCalculatorGUI/Configuration/CatchDifficultyTuningParameters.cs
@@ -16,7 +16,7 @@ namespace PerformanceCalculatorGUI.Configuration
                 minValue: 0.8, maxValue: 1.2),
             doubleParam(nameof(CatchDifficultyConstants.SrPreMultiplier), t =>  t.SrPreMultiplier, (t, v) => t with { SrPreMultiplier = v },
                 minValue: 0.05, maxValue: 2.0),
-            doubleParam(nameof(CatchDifficultyConstants.SrPostMultiplier), t =>  t.SrPreMultiplier, (t, v) => t with { SrPreMultiplier = v },
+            doubleParam(nameof(CatchDifficultyConstants.SrPostMultiplier), t =>  t.SrPostMultiplier, (t, v) => t with { SrPostMultiplier = v },
                 minValue: 0.05, maxValue: 2.0),
             doubleParam(nameof(CatchDifficultyConstants.SrOffset), t =>  t.SrOffset, (t, v) => t with { SrOffset = v },
                 minValue: 0.05, maxValue: 2.0),
